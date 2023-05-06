ec62dbc10ad297535de6f0c4c1e6c4c1a2c6cbfb
warengeorge <georgeomiebi@gmail.com>
2023-05-07 00:43:29 +0100

    Added async_generator coroutine

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" async_generator coroutine """
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async with yeild operations """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
